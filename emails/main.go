f7fa330e27344a9d3b308d369fbad5b77655c617
Keyur Shah <keyurshah3939@gmail.com>
2023-11-04 15:53:06 +0530

    File Completed

--- emails/main.go
+++ emails/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net/mail"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -26,12 +27,14 @@ type Config struct {
 	ReadEmailTextFromCsv  bool   `json:"readEmailTextFromCsv" validate:"boolean"`
 	IsEmailBodyHTML       bool   `json:"isEmailBodyHTML" validate:"boolean"`
 	EmailBody             string `json:"emailBody" `
-	ParallelEmails        int    `json:"parallelEmails" validate:"min=1"`
+	ParallelEmails        int    `json:"parallelEmails" validate:"min=2"`
 	InputFileName         string `json:"inputFile"`
 	Host                  string `json:"host" validate:"required"`
 	Port                  int    `json:"port" validate:"required,port"`
 	UserName              string `json:"userName" validate:"required"`
 	Password              string `json:"password" validate:"required"`
+	FromEmail             string `json:"fromEmail" validate:"required"`
+	FromName              string `json:"fromName" validate:"required"`
 }
 
 var currentDir = ""
@@ -159,6 +162,7 @@ func main() {
 		check(err)
 	}
 	configFilePAth := filepath.Join(currentDir, "configs.json")
+	fmt.Printf("Config path %s\n", configFilePAth)
 	if _, err := os.Stat(configFilePAth); errors.Is(err, os.ErrNotExist) {
 		panic(fmt.Errorf("Config Not Exist on Path %s", configFilePAth))
 	}
@@ -209,13 +213,105 @@ func Emails() {
 	inputBytes, err := os.ReadFile(InputFilePath)
 	check(err)
 	input := string(inputBytes)
+	_, err = os.Create(OutPutFilePath) //create a new file
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	RowsData := strings.Split(input, "\n")
 	fmt.Printf("total %d Rows Found\n", len(RowsData))
+	currentQueCount := 0
+	// var wg sync.WaitGroup
 	for _, row := range RowsData {
 		cols := strings.Split(row, ",")
-		if len(cols) < 2 {
-			AppendToOutPutFile("Cells Length < 2 Found\n")
+		checkCells := 1
+		if ThisConfig.ReadAttachmentFromCSV {
+			checkCells = 2
+		}
+		if ThisConfig.ReadSubjectFromCsv {
+			checkCells = 3
+		}
+		if ThisConfig.ReadEmailTextFromCsv {
+			checkCells = 5
+		}
+		if len(cols) < checkCells {
+			AppendToOutPutFile(fmt.Sprintf("Cells Length < %d Found\n", checkCells))
 			continue
 		}
+		address := strings.TrimSpace(cols[0])
+		if _, err := mail.ParseAddress(address); err != nil {
+			AppendToOutPutFile(fmt.Sprintf("%s,false,Invalid Email\n", address))
+			continue
+		}
+		attacheMentName := strings.TrimSpace(ThisConfig.FixedAttachmentName)
+		if ThisConfig.ReadAttachmentFromCSV {
+			attacheMentName = strings.TrimSpace(cols[1])
+		}
+		email := gomail.NewMessage()
+		if attacheMentName != "" {
+			attacheMentNamePath := filepath.Join(ThisConfig.BasePathForAttachment, attacheMentName)
+			if _, err := os.Stat(attacheMentNamePath); errors.Is(err, os.ErrNotExist) {
+				AppendToOutPutFile(fmt.Sprintf("%s,false,File Path Not Exists %s\n", address, attacheMentNamePath))
+				continue
+			}
+			email.Attach(attacheMentNamePath)
+		}
+		emailSubject := ThisConfig.EmailSubject
+		if ThisConfig.ReadSubjectFromCsv {
+			emailSubject = strings.TrimSpace(cols[2])
+		}
+		if emailSubject == "" {
+			AppendToOutPutFile(fmt.Sprintf("%s,false,Email Subject Not Found\n", address))
+			continue
+		}
+		email.SetHeaders(map[string][]string{
+			"From":    {email.FormatAddress(ThisConfig.FromEmail, ThisConfig.FromName)},
+			"To":      {address},
+			"Subject": {emailSubject},
+		})
+		emailBody := ThisConfig.EmailBody
+		if ThisConfig.ReadEmailTextFromCsv {
+			emailBody = strings.TrimSpace(cols[3])
+		}
+		if emailBody == "" {
+			AppendToOutPutFile(fmt.Sprintf("%s,false,Email Body Not Found\n", address))
+			continue
+		}
+		isHtml := ThisConfig.IsEmailBodyHTML
+		if ThisConfig.ReadEmailTextFromCsv {
+			localBody := strings.ToLower(strings.TrimSpace(cols[4]))
+			if localBody == "true" {
+				isHtml = true
+			}
+		}
+		if isHtml {
+			email.SetBody("text/html", emailBody)
+		} else {
+			email.SetBody("text/plain", emailBody)
+		}
+		currentQueCount++
+		// wg.Add(1)
+		// go func() {
+		fmt.Printf("Sending Mail to %s\n", address)
+
+		// Send Mail
+
+		err := gomail.Send(authenticated, email)
+		if err != nil {
+			fmt.Printf("Mail Sending to %s failed \n", address)
+			AppendToOutPutFile(fmt.Sprintf("%s,false,Something Went Wrong %#v\n", address, err))
+		} else {
+			fmt.Printf("Mail Send to %s\n", address)
+			AppendToOutPutFile(fmt.Sprintf("%s,true\n", address))
+		}
+
+		// wg.Done()
+		// }()
+		if currentQueCount == ThisConfig.ParallelEmails {
+			currentQueCount = 0
+			// wg.Wait()
+		}
 	}
+	// wg.Wait()
+	// close()
 }
